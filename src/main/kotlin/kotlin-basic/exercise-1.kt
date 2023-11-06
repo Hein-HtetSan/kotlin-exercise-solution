1eaf052974a480cf7084bda7601ad4112c2b0d7d
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-11-06 13:45:17 +0630

    exercise-1 solution

---
+++ src/main/kotlin/kotlin-basic/exercise-1.kt
@@ -0,0 +1,13 @@
+package `kotlin-basic`
+
+fun main(){
+    val integer: Int = 42
+    val double: Double = 3.14
+    val string: String = "Hello, Kotlin"
+    val boolean: Boolean = true
+
+    println(integer)
+    println(double)
+    println(string)
+    println(boolean)
+}
