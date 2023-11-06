7fda6504778277504e013fced1b3a1aa7416e0c8
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-11-06 12:56:58 +0630

    exercise-1 solution

---
+++ src/main/kotlin/control-flow/exercise-1.kt
@@ -0,0 +1,12 @@
+package `control-flow`
+
+fun main(){
+    var num = 4
+    if(num > 0){
+        println("$num is positive")
+    }else if(num < 0){
+        println("$num is negative")
+    }else{
+        println("$num is zero")
+    }
+}
