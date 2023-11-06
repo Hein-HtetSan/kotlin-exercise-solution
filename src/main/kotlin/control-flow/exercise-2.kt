0bdde10e054f22682e3f09014213e0d0faa1c379
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-11-06 12:27:07 +0630

    first commit

---
+++ src/main/kotlin/control-flow/exercise-2.kt
@@ -0,0 +1,10 @@
+package `control-flow`
+
+fun main(){
+    var num: Int = 74
+    if(num%7 == 0){
+        println("$num is divisible by 7")
+    }else{
+        println("$num isn't divisible by 7")
+    }
+}
