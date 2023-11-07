ba4e04966f15818daabb76ee491cdf1e7dd5bdde
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-11-07 13:00:21 +0630

    exercise-2 solution

---
+++ src/main/kotlin/kotlin-basic/exericese-3.kt
@@ -0,0 +1,16 @@
+package `kotlin-basic`
+
+fun main(args: Array<String>){
+
+    val length = 4
+    val width = 5
+    // calculating Rectangular Area
+    val area = length * width
+    println("Area of rectangular : " + area)
+
+    // Calculating Rectangle Perimeter
+    val perimeter = 2 * (length + width)
+    println("Perimeter of rectangle : " + perimeter)
+
+}
+
